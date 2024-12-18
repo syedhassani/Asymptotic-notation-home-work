4b4e562797a0593e35b23cc68afc6df4e33b2629
syedhassani <syedmhassani12@gmail.com>
2024-12-18 20:40:14 +1100

    homework

---
+++ main.py
@@ -0,0 +1,15 @@
+def printnumber(n):
+#     literation = 0
+#     print("the total number my user is",n)
+#     literation+=1
+#     print("total literation done by computer is",literation,"\n")
+# printnumber(10)
+# printnumber(200)
+def OnTime(n):
+    iteration=0
+    for i in range(1,n+1):
+        iteration+=1
+    print("when n is ",n," iteration =",iteration)
+OnTime(10)
+OnTime(20)
+OnTime(30)
